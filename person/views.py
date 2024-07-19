fcd5c0e3e2888e19a590166941dcb43a00f2f6a6
Firdavs <firdavsxudoyqulov098@gmail.com>
2024-07-19 15:22:21 +0500

    crud

--- person/views.py
+++ person/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from person.models import Food
@@ -12,6 +13,7 @@ class Createfood(generics.CreateAPIView):
     serializer_class = FoodSerializers
     permission_classes = [IsAuthenticated]
 
+
 class listfood(generics.ListAPIView):
     queryset = Food.objects.all()
     serializer_class = FoodSerializers
@@ -21,12 +23,14 @@ class Updatefood(generics.UpdateAPIView):
     queryset = Food.objects.all()
     serializer_class = FoodSerializers
     permission_classes = [IsOwnerUserOrReadOnly]
+    authentication_classes = [TokenAuthentication]
 
 
 class deletefood(generics.DestroyAPIView):
     queryset = Food.objects.all()
     serializer_class = FoodSerializers
     permission_classes = [IsAdminUser]
+    authentication_classes = [TokenAuthentication]
 
 class createcategory(generics.CreateAPIView):
     queryset = Food.objects.all()
@@ -42,9 +46,11 @@ class Updatecategory(generics.UpdateAPIView):
     queryset = Food.objects.all()
     serializer_class = CategorySerializers
     permission_classes = [IsOwnerUserOrReadOnly]
+    authentication_classes = [TokenAuthentication]
 
 
 class deletecategory(generics.DestroyAPIView):
     queryset = Food.objects.all()
     serializer_class = CategorySerializers
     permission_classes = [IsAdminUser]
+    authentication_classes = [TokenAuthentication]
